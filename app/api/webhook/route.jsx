bf0e292989a7293e373b651e7c0b7a9de3d07e5a
RR <rajko.fed@gmail.com>
2023-08-13 22:33:33 +0200

    s

--- app/api/webhook/route.jsx
+++ app/api/webhook/route.jsx
@@ -3,12 +3,6 @@ import { buffer } from "micro";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
 export default async function handler(req, res) {
   if (req.method == "POST") {
     let event;
