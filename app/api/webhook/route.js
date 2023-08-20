c70fcd2f51d3763fb3eea81a5a881e5729f3641a
RR <rajko.fed@gmail.com>
2023-08-20 18:05:31 +0200

    test

---
+++ app/api/webhook/route.js
@@ -0,0 +1,111 @@
+import Stripe from "stripe";
+import { buffer } from "micro";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(req, res) {
+  // const sendEmail = (recipient, subject, message) => {
+  //   const emailParams = {
+  //     from_name: "ORDERS-RAW VEGAN CAKE",
+  //     to_email: recipient,
+  //     subject: subject,
+  //     message_html: message,
+  //   };
+
+  //   emailjs.send("service_9ey5sol", "template_keyya3g", emailParams)
+  //     .then((response) => {
+  //       console.log("Email sent:", response);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Email error:", error);
+  //     });
+  //   };
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+  const fulfillOrder = (session) => {
+    // TODO: fill me in
+    console.log("Fulfilling order", session);
+  };
+
+  if (req.method == "POST") {
+    let event;
+
+    try {
+      const rawBody = await buffer(req);
+      const signature = req.headers["stripe-signature"];
+
+      event = stripe.webhooks.constructEvent(
+        rawBody.toString(),
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.log(`X Error message: ${err.message}`);
+      res.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    switch (event.type) {
+      case "checkout.session.completed": {
+        const session = event.data.object;
+        // Save an order in your database, marked as 'awaiting payment'
+
+        // Check if the order is paid (for example, from a card payment)
+        //
+        // A delayed notification payment will have an `unpaid` status, as
+        // you're still waiting for funds to be transferred from the customer's
+        // account.
+        if (session.payment_status === "paid") {
+          fulfillOrder(session);
+        }
+
+        break;
+      }
+
+      case "checkout.session.async_payment_succeeded": {
+        const session = event.data.object;
+
+        // Fulfill the purchase...
+        fulfillOrder(session);
+
+        break;
+      }
+
+      case "checkout.session.async_payment_failed": {
+        const session = event.data.object;
+
+        // Send an email to the customer asking them to retry their order
+        emailCustomerAboutFailedPayment(session);
+
+        break;
+      }
+
+      case "payment_intent.succeeded": {
+        const session = event.data.object;
+
+        // Fulfill the purchase...
+        fulfillOrder(session);
+
+        break;
+      }
+
+      case "payment_intent.payment_failed": {
+        const session = event.data.object;
+
+        // Send an email to the customer asking them to retry their order
+        emailCustomerAboutFailedPayment(session);
+
+        break;
+      }
+    }
+    res.json({ received: true });
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
